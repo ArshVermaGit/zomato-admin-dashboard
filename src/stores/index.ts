e1f5afabad53fb67a01d73a63fd538fe25b44630
Arsh Verma <arshverma.dev@gmail.com>
2025-12-15 15:14:42 +0530

    Create index.ts

---
+++ src/stores/index.ts
@@ -0,0 +1,5 @@
+// Re-export all stores for easy access
+export { useAuthStore } from './authStore';
+export { useOrdersStore } from './ordersStore';
+export { useUIStore } from './uiStore';
+export { useNotificationsStore } from './notificationsStore';
