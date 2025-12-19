cb5419f75db17668b5446a2865b55192d5e1be57
Arsh Verma <arshverma.dev@gmail.com>
2025-12-19 17:28:17 +0530

    Update GeneralSettings.tsx

--- src/components/dashboard/settings/GeneralSettings.tsx
+++ src/components/dashboard/settings/GeneralSettings.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
+
 
 export function GeneralSettings() {
     return (
